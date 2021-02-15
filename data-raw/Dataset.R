d3e080dbefca1a3546ac4da283b7b174afd5f159
Lauren Rabe <lrabe@reed.edu>
2021-02-14 18:39:56 -0800

    made the columns better

--- data-raw/Dataset.R
+++ data-raw/Dataset.R
@@ -1,7 +1,9 @@
 ## code to prepare `~/pkgGrpr/city_data.csv` dataset goes here
 
 install.packages(tidyverse)
+install.packages(lubridate)
 library(tidyverse)
+library(lubridate)
 
 india_air <- readr::read_csv("city_day.csv")
 
@@ -10,9 +12,8 @@ india_air <- india_air %>%
                      "Bengluru", "Chennai", "Ahmedabad",
                      "Mumbai")) %>%
   dplyr::select(-PM10, -NH3) %>%
-  dplyr::mutate(date = lubridate::ymd(Date)) %>%
-  dplyr::select(-Date) %>%
-  dplyr::rename(city = City, benzene = Benzene,
+  dplyr::mutate(Date = lubridate::ymd(Date)) %>%
+  dplyr::rename(city = City, benzene = Benzene, date = Date,
                 toluene = Toluene, xylene = Xylene,
                 AQI_bucket = AQI_Bucket)
 
