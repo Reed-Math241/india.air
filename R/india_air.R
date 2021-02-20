7ddba47418291703c5c7ac156b8e9b935b3e914b
Lauren Rabe <lrabe@reed.edu>
2021-02-19 19:38:55 -0800

    added data source to help file

--- R/india_air.R
+++ R/india_air.R
@@ -19,6 +19,8 @@
 #'   \item{AQI}{Air Quality Index (numeric)}
 #'   \item{AQI_bucket}{Description of AQI(character)(Severe, Very Poor, Poor, Moderate, Satisfactory, Good)}
 #' }
+#' @source {Compiled public domain dataset from kaggle.com} \url{https://www.kaggle.com/rohanrao/air-quality-data-in-india}
+#' @source {Data extracted from the Central Pollution Control Board of India} \url {https://cpcb.nic.in/}
 #' @source {National Air Quality Standards in India as of 2009} \url{https://cpcb.nic.in/uploads/National_Ambient_Air_Quality_Standards.pdf}
 #' @source {National Air Quality Index Definitions} \url{https://cpcb.nic.in/displaypdf.php?id=bmF0aW9uYWwtYWlyLXF1YWxpdHktaW5kZXgvQWJvdXRfQVFJLnBkZg==}
 #' @source {List of CAAQM Stations Installed Across India} \url{https://app.cpcbccr.com/caaqms/download?filename=Station_List.xlsx}
